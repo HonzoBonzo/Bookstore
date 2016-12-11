87fe119f04811dcbb44bb94f4191e39adc307810
HonzoBonzo <konradb12@gmail.com>
2016-12-11 14:49:46 +0100

    first commit

---
+++ Book.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetBookstore
+{
+    class Book
+    {
+        string 
+            name,
+            category,
+            publisher,
+            author;
+        int yearOfRelease;
+
+        public Book()
+        {
+            category = null;
+            publisher = null;
+            author = null;
+            yearOfRelease = -1;
+        }
+
+        public Book(string nam, string cat, string pub, string aut, int year) {
+            name = nam;
+            category = cat;
+            publisher = pub;
+            author = aut;
+            yearOfRelease = year;
+        }
+
+        public Book clone()
+        {
+            Book b = new Book(this.name, this.category, this.publisher, this.author, this.yearOfRelease);
+            return b;
+        }
+
+        public void show() {
+            Console.WriteLine("Book: {0}, Category: {1}, Publisher: {2}, Author: {3}, Year of release: {4}",name,category,publisher,author,yearOfRelease);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetBookstore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book ksiazka = new Book("Krzyzacy", "Powieść historyczna", "WW", "Henryk Sienkiewicz", 1900);
+            ksiazka.show();
+            Book ksiazkaDuplicate = ksiazka.clone();
+            ksiazkaDuplicate.show();
+
+            Console.WriteLine(ksiazka.Equals(ksiazkaDuplicate));
+            Console.WriteLine(ksiazka == ksiazkaDuplicate);
+
+            ksiazka = ksiazkaDuplicate;
+            Console.WriteLine(ksiazka.Equals(ksiazkaDuplicate));
+            Console.WriteLine(ksiazka == ksiazkaDuplicate);
+
+            Console.ReadKey();
+
+        }
+    }
+}
